eb1f5e5910012f0db5bbc8d8be9b4f667070048e
Chad Chapman <CChapman36820@tacomacc.edu>
2017-07-09 23:21:18 -0700

    python refresher exercises
    basic syntax exercises, nice refresh for python

---
+++ mod1mod2mod3.py
@@ -0,0 +1,160 @@
+mod 1
+
+# Example, do not modify!
+print(5 / 8)
+
+# Put code below here
+print(7+10)
+
+##
+
+# Just testing division
+print(5 / 8)
+
+# Addition works too.
+print(7 + 10)
+
+##
+
+# Addition and subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication and division
+print(3 * 5)
+print(10 / 2)
+
+# Exponentiation
+print(4 ** 2)
+
+# Modulo
+print(18 % 7)
+
+# How much is your $100 worth after 7 years?
+print(100*(1.1**7))
+
+##
+
+mod2
+
+# Create a variable savings
+savings = 100
+
+# Print out savings
+print(savings)
+
+##
+
+# Create a variable savings
+savings = 100
+
+# Create a variable factor
+factor = 1.10
+
+# Calculate result
+result = savings * (factor**7)
+
+# Print out result
+print(result)
+
+##
+
+# Create a variable desc
+desc = "compound interest"
+
+# Create a variable profitable
+profitable = True
+
+##
+
+# Several variables to experiment with
+savings = 100
+factor = 1.1
+desc = "compound interest"
+
+# Assign product of factor and savings to year1
+year1 = factor * savings
+
+# Print the type of year1
+print(type(year1))
+
+# Assign sum of desc and desc to doubledesc
+doubledesc = desc + desc
+
+# Print out doubledesc
+print(doubledesc)
+
+##
+
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
+
+##
+
+mod3
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Create list areas
+areas = [hall, kit, liv, bed, bath]
+
+# Print areas
+print(areas)
+
+
+##
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Adapt list areas
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0,
+"bedroom", 10.75, "bathroom", 9.50]
+
+# Print areas
+print(areas)
+
+##
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# house information as list of lists
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv],
+         ["bedroom", bed],
+         ["bathroom", bath]]
+
+# Print out house
+print(house)
+
+# Print out the type of house
+print(type(house))
+
+##
+
+
